b6a71358ab2ea75e33c41d466ee17f6605648b37
Travis <travis@teamcoco.com>
2012-11-07 00:54:39 +0000

    fixed ws

--- src/bolt/render.php
+++ src/bolt/render.php
@@ -20,64 +20,64 @@ class render extends plugin {
 
     // global args
     private $_globals = array();
-    
+
     // modules
     private $_modules = array();
 
     // default
     public function _default($args=array()) {
-    
+
         // render a template
         if (isset($args['template'])) {
             $tmpl = $args['template']; unset($args['template']);
             return $this->template($tmpl, $args, $args['view']);
         }
-        
+
         // render a string
         else if (isset($args['string'])) {
-            $str = $args['string']; unset($args['string']);        
+            $str = $args['string']; unset($args['string']);
             return $this->string($str, $args, $args['view']);
         }
-        
+
         // render a module
         else if (isset($args['module'])) {
-            $mod = $args['module']; unset($args['module']);                
+            $mod = $args['module']; unset($args['module']);
             return $this->module($mod, $args, $args['view']);
         }
-        
+
         // who the hell knows
         // assume the args are just args
         else {
-                        
+
             // this is em
             return $this;
-            
+
         }
-        
+
     }
 
     // __get
     public function __get($key) {
         return $this->get($key);
     }
-    
+
     // __set
     public function __set($key, $value) {
         $this->set($key, $value);
     }
-    
+
     public function get($key) {
         return (array_key_exists($key, $this->_globals) ? $this->_globals[$key] : false);
     }
-    
-    // global 
+
+    // global
     public function set() {
         if (is_string(func_get_arg(0))) {
             $key = func_get_arg(0);
-            $value = func_get_arg(1);        
+            $value = func_get_arg(1);
             $this->_globals[$key] = $value;
         }
-        else {        
+        else {
             foreach (func_get_args() as $array) {
                 foreach ($array as $key => $valuee) {
                     $this->_globals[$key] = $value;
@@ -87,37 +87,37 @@ class render extends plugin {
     }
 
     public function register($name, $class, $type='factory') {
-      
-        // add it 
+
+        // add it
         $this->_modules[$name] = array(
             'class' => $class,
             'type' => $type
         );
-        
-    }    
 
-    public function moduleExists($name) {        
+    }
+
+    public function moduleExists($name) {
         return array_key_exists($name, $this->_modules);
     }
-    
+
     public function getModule($name, $args=array()) {
-     
+
         // if this module doesn't exist
-        if (!$this->moduleExists($name)) { return; }     
-     
+        if (!$this->moduleExists($name)) { return; }
+
         // factory or singleton
         if ($this->_modules[$name]['type'] == 'singleton') {
             if (!array_key_exists('instance', $this->_modules[$name])) {
                 $v = $this->_modules[$name]['instance'] = new $this->_modules[$name]['class']($args);
-            }                            
+            }
         }
         else {
             $v = new $this->_modules[$name]['class']($args);
-        }     
-        
-        // return it        
+        }
+
+        // return it
         return $v;
-        
+
     }
 
     public function getModules() {
@@ -128,7 +128,7 @@ class render extends plugin {
 
         // if this module doesn't exist
         if (!$this->moduleExists($name)) { return; }
-    
+
         // get module
         $v = $this->getModule($name, $args);
 
@@ -142,16 +142,16 @@ class render extends plugin {
 
         // now render the output as a string
         return $v->execute('module')->getContent();
-    
+
     }
 
-    public function template($tmpl, $args=array()) {    
-            
+    public function template($tmpl, $args=array()) {
+
         // our file name
         $file = $tmpl;
-            
+
             // end
-            if (stripos($tmpl, '.template.php') === false){ 
+            if (stripos($tmpl, '.template.php') === false){
                 $file .= '.template.php';
             }
 
@@ -159,7 +159,7 @@ class render extends plugin {
         $vars = p_raw('vars', array(), $args);
 
             // if we have a view
-            if(array_key_exists('view', $args)) {                
+            if(array_key_exists('view', $args)) {
                 $vars = array_merge($vars, $args['view']->getParams()->getData());
                 $vars['view'] = $args['view'];
             }
@@ -172,7 +172,7 @@ class render extends plugin {
                 if (is_array($v)) {
                     $vars[$k] = b::bucket($v);
                 }
-            }            
+            }
 
         // content
         $content = call_user_func(function($__my_file, $vars){
@@ -183,7 +183,7 @@ class render extends plugin {
             // define all
             foreach ( $vars as $k => $v ) {
                 $$k = $v;
-            }       
+            }
 
             // include the page
             include($__my_file);
@@ -195,22 +195,22 @@ class render extends plugin {
             ob_clean();
 
             // return
-            return $page;                    
+            return $page;
 
-        }, $file, $vars);    
-        
+        }, $file, $vars);
+
     		// give it back
     		return $this->string($content, $args);
-    
+
     }
 
     public function string($str, $args=array()) {
-    
+
         // vars
         $vars = p_raw('vars', array(), $args);
 
             // if we have a view
-            if(array_key_exists('view', $args)) {                
+            if(array_key_exists('view', $args)) {
                 $vars = array_merge($vars, $args['view']->getParams()->getData());
                 $vars['view'] = $args['view'];
             }
@@ -223,39 +223,39 @@ class render extends plugin {
                 if (is_array($v)) {
                     $vars[$k] = b::bucket($v);
                 }
-            }            
+            }
 
       // what renderer
       $renderer = p('renderer', 'mustache', $args);
 
-      // render it 
+      // render it
       $str =  b::render($renderer)->render($str, $vars);
 
       // str
       return $str;
 
-    
+
   //       // let's find some variables
   //       if (preg_match_all('/\{(\$[^\}]+)\}/', $str, $matches, PREG_SET_ORDER)) {
-            
+
   //           // loop
-  //           foreach ($matches as $match) {        
+  //           foreach ($matches as $match) {
 
   //               // replace any . in the string
-  //               // since they should be 
+  //               // since they should be
   //               $var = trim(str_replace('.', "->", $match[1]),'$');
-                
+
   //               // tok the first str
   //               $parts = explode("->", $var);
-                
+
   //               // first
   //               $first = array_shift($parts);
-                
+
   //               // val
   //               $val = "";
-                
-  //               // value        
-  //               if (array_key_exists($first, $vars)) {                
+
+  //               // value
+  //               if (array_key_exists($first, $vars)) {
   //                   $val = eval('return '.implode('->', array_merge(array('$vars[$first]'),$parts)).";");
   //               }
 
@@ -263,105 +263,105 @@ class render extends plugin {
   //               if (!is_string($val) AND !is_numeric($val)) {
   //                   $val = "";
   //               }
-                
-  //               // replace it 
-  //               $str = preg_replace("#".preg_quote($match[0], '#')."#", $val, $str, 1);
-                
-  //           }
-            
-  //       }
-        
 
-    
+  //               // replace it
+  //               $str = preg_replace("#".preg_quote($match[0], '#')."#", $val, $str, 1);
+
+  //           }
+
+  //       }
+
+
+
   //       // first sanatize out our string
   //       $str = str_replace("\}", "%##%", $str);
-        
+
   //       // exec
   //       $exec = $modules = array();
-            	
+
   //   	// take the string and look for any functions
-  //   	// functions always start with a b:, so that's what 
+  //   	// functions always start with a b:, so that's what
   //   	// we'll key off of
 		// if ( preg_match_all("/\{(b::?[^\}]+)\}/i", $str, $matches, PREG_SET_ORDER) ) {
-  //           $exec = array_merge($exec, $matches);  
+  //           $exec = array_merge($exec, $matches);
   //       }
 
   //   	// now find direct function calls
-		// if ( preg_match_all("/\{\!([^\}]+)\!\}/i", $str, $matches, PREG_SET_ORDER) ) {    
-  //           $exec = array_merge($exec, $matches);  
-  //       }            
-    	
-  //   	// now modules
-		// if ( preg_match_all("/\{\%([^\}]+)\%\}/i", $str, $matches, PREG_SET_ORDER) ) {    
-  //           $modules = $matches;              
+		// if ( preg_match_all("/\{\!([^\}]+)\!\}/i", $str, $matches, PREG_SET_ORDER) ) {
+  //           $exec = array_merge($exec, $matches);
   //       }
-        
+
+  //   	// now modules
+		// if ( preg_match_all("/\{\%([^\}]+)\%\}/i", $str, $matches, PREG_SET_ORDER) ) {
+  //           $modules = $matches;
+  //       }
+
   //       // make sure everything is an object
   //       foreach ($vars as $k => $v) {
   //           if (is_array($v)) {
   //               $vars[$k] = new \bolt\dao\item($v);
   //           }
-  //       }    
-		
-      
-		
+  //       }
+
+
+
 
 		// // define all
 		// foreach ( $vars as $k => $v ) {
 		// 	$$k = $v;
-		// }		
-		
+		// }
+
 		// // view
 		// $view = $this;
 
   //       // anything to exec
   //       foreach ($exec as $stuff) {
-            
+
   //           // some things
-  //           $match = $stuff[0];            
+  //           $match = $stuff[0];
   //           $func = $stuff[1];
-                
+
   //           // clean the func
   //           $func = "return ".str_replace("%##%", "}", $func).";";
-            
+
   //           // we need to buffer the output
   //           ob_start();
-                
+
   //               // html
   //               try { $resp = eval($func); }
   //               catch (Exception $e) {}
-                    
+
   //   		// stop
   //   		$out = ($resp !== null ? $resp : ob_get_contents());
-    
+
   //   		// clean
-  //   		ob_clean();                
-            
+  //   		ob_clean();
+
   //           // replace our match
-  //           $str = preg_replace("#".preg_quote($match, '#')."#", $out, $str, 1);		  
-		
-		// }    	
-		
-	//	var_dump($str); 
-    	
+  //           $str = preg_replace("#".preg_quote($match, '#')."#", $out, $str, 1);
+
+		// }
+
+	//	var_dump($str);
+
     	// return a string
     	return $str;
-    
-    } 
-    
+
+    }
+
     public function render($view, $args=array()) {
-        
+
         // view
-        $accept = p('accept', false, $args);        
-        
+        $accept = p('accept', false, $args);
+
         // execute
-        $view = $view->execute(false, $accept);        
-        
-        
-    
+        $view = $view->execute(false, $accept);
+
+
+
         // get that crap
         return $p->render($view);
-        
+
     }
 
 }
--- src/bolt/source/webservice.php
+++ src/bolt/source/webservice.php
@@ -20,21 +20,21 @@ class webservice extends \bolt\plugin\factory {
         'curlOpts'  => array(),
         'timeout'   => 10
     );
-    
+
     // oauth
     private $_oauth = false;
     private $_curl = false;
     private $_last = false;
-    
+
     // construct
-    public function __construct($cfg=array()) {    
+    public function __construct($cfg=array()) {
         $this->_config = array_merge($this->_config, $cfg);
     }
-    
+
     public function getLastResponse() {
         return $this->_last;
     }
-    
+
     public function config($key, $val=false) {
         if (is_array($key)) {
             foreach ($key as $k => $v) {
@@ -48,196 +48,196 @@ class webservice extends \bolt\plugin\factory {
             return $this->__set($key);
         }
     }
-    
+
     // set and get
     public function __get($name) {
         return (array_key_exists($name, $this->_config) ? $this->_config[$name] : false);
     }
-    
+
     // set
     public function __set($name, $value=false) {
         return ($this->_config[$name] = $value);
     }
-    
+
     // request
     public function request() {
-    
+
         // args
         $args = func_get_args();
-    
+
         // url
-        $args[0] = "{$this->protocol}://{$this->host}".($this->port!=80?":{$this->port}":"")."/".ltrim($args[0],'/');    
-            
+        $args[0] = "{$this->protocol}://{$this->host}".($this->port!=80?":{$this->port}":"")."/".ltrim($args[0],'/');
+
         // route to proper func
         switch($this->method) {
-        
+
             // straight curl request
             case 'curl':
                 return call_user_func_array(array($this, 'curlRequest'), $args);
-                
+
             // oauth request
             case 'oauth':
                 return call_user_func_array(array($this, 'oauthRequest'), $args);
-                
+
         };
-    
+
     }
-    
+
     public function getCurl() {
         return $this->_curl;
     }
     public function getOauth() {
         return $this->_oauth;
     }
-    
-    protected function curlRequest($url, $params=array(), $method="GET", $headers=array(), $useIniFilePost=false) {    
-    
+
+    protected function curlRequest($url, $params=array(), $method="GET", $headers=array(), $useIniFilePost=false) {
+
 		// headers
-		$headers = array_merge($this->headers, $headers);     
-    
-        // add our params 
+		$headers = array_merge($this->headers, $headers);
+
+        // add our params
         if ( $method == 'GET' ) {
             $url = b::addUrlParams($url, $params);
         }
-                
+
         // new curl request
         $this->_curl = curl_init();
 
         // set some stuff
         curl_setopt($this->_curl, CURLOPT_URL, $url);
-        curl_setopt($this->_curl, CURLOPT_HEADER, 0);    
+        curl_setopt($this->_curl, CURLOPT_HEADER, 0);
         curl_setopt($this->_curl, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($this->_curl, CURLOPT_CONNECTTIMEOUT,5);        
-        curl_setopt($this->_curl, CURLOPT_CUSTOMREQUEST, $method);        
+        curl_setopt($this->_curl, CURLOPT_CONNECTTIMEOUT,5);
+        curl_setopt($this->_curl, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($this->_curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($this->_curl, CURLOPT_SSL_VERIFYPEER, 0);        
-        curl_setopt($this->_curl, CURLINFO_HEADER_OUT, 1);    
+        curl_setopt($this->_curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($this->_curl, CURLINFO_HEADER_OUT, 1);
         curl_setopt($this->_curl, CURLOPT_TIMEOUT, $this->timeout);
-        
+
         // cookies
         if (array_key_exists('cookies', $headers)) {
-        
+
             // cookies
             $cookies = array();
-        
+
             // headers
             foreach ($headers['cookies'] as $k => $v) {
                 $cookies[] = "$k=$v";
             }
-            
+
             // cookies
-            curl_setopt($this->_curl, CURLOPT_COOKIE, implode(';',$cookies));            
+            curl_setopt($this->_curl, CURLOPT_COOKIE, implode(';',$cookies));
             unset($headers['cookies']);
-            
-        }				
-				
+
+        }
+
         // add params
         if ( $method == 'POST' ) {
-        
+
             if ($useIniFilePost) {
-                
-                // write our params to tmp       
+
+                // write our params to tmp
                 $fp = fopen('php://temp/maxmemory:256000', 'w');
                 fwrite($fp, $params);
-                fseek($fp, 0); 
-                
+                fseek($fp, 0);
+
                 // length
                 $len = strlen($params);
-                
-                // put it 
-                curl_setopt($this->_curl, CURLOPT_BINARYTRANSFER, true);            
-                curl_setopt($this->_curl, CURLOPT_INFILE, $fp);                
-                curl_setopt($this->_curl, CURLOPT_INFILESIZE, $len);       
-                curl_setopt($this->_curl, CURLOPT_POST, TRUE);                                
-            
+
+                // put it
+                curl_setopt($this->_curl, CURLOPT_BINARYTRANSFER, true);
+                curl_setopt($this->_curl, CURLOPT_INFILE, $fp);
+                curl_setopt($this->_curl, CURLOPT_INFILESIZE, $len);
+                curl_setopt($this->_curl, CURLOPT_POST, TRUE);
+
             }
             else {
-            
+
                 // post
     	        curl_setopt($this->_curl, CURLOPT_POSTFIELDS, http_build_query($params));
                 curl_setopt($this->_curl, CURLOPT_POST, TRUE);
             }
-	        
+
         }
-        if ($method == 'PUT' ) {	        
-        
+        if ($method == 'PUT' OR $method == 'DELETE') {
+
             // if params is an array
             if (is_array($params)) {
                 $params = http_build_query($params);
             }
-                    
-                // write our params to tmp       
+
+                // write our params to tmp
                 $fp = fopen('php://temp/maxmemory:256000', 'w');
                 fwrite($fp, $params);
-                fseek($fp, 0); 
-                
+                fseek($fp, 0);
+
                 // length
                 $len = strlen($params);
-                
-                // put it 
-                curl_setopt($this->_curl, CURLOPT_BINARYTRANSFER, true);            
-                curl_setopt($this->_curl, CURLOPT_PUT, TRUE);                
-                curl_setopt($this->_curl, CURLOPT_INFILE, $fp);                
-                curl_setopt($this->_curl, CURLOPT_INFILESIZE, $len);                
-                
+
+                // put it
+                curl_setopt($this->_curl, CURLOPT_BINARYTRANSFER, true);
+                curl_setopt($this->_curl, CURLOPT_PUT, TRUE);
+                curl_setopt($this->_curl, CURLOPT_INFILE, $fp);
+                curl_setopt($this->_curl, CURLOPT_INFILESIZE, $len);
+
         }
-        
+
         // auth
         if ( isset($this->auth['username']) ) {
         	curl_setopt($this->_curl, CURLOPT_USERPWD, "{$this->auth['username']}:{$this->auth['password']}");
-        }		
-        						
+        }
+
         // add headers
-        curl_setopt($this->_curl,CURLOPT_HTTPHEADER, array_map(function($k, $v){ 
+        curl_setopt($this->_curl,CURLOPT_HTTPHEADER, array_map(function($k, $v){
             if ($k AND $v) {
             	return "$k:$v";
             }
         },array_keys($headers), $headers));
-        
+
         // curl ops
-        if ($this->curlOpts) {        
+        if ($this->curlOpts) {
             curl_setopt_array($this->_curl, $this->curlOpts);
         }
-        
+
         // make the request
-        $result = curl_exec($this->_curl);   
-            
-        // return our re    
+        $result = curl_exec($this->_curl);
+
+        // return our re
         $r = $this->_last = new webserviceResponse(
             $this,
-            $result, 
-            curl_getinfo($this->getCurl(),CURLINFO_HTTP_CODE), 
+            $result,
+            curl_getinfo($this->getCurl(),CURLINFO_HTTP_CODE),
             curl_getinfo($this->getCurl())
         );
 
         // close our curl
-        curl_close($this->_curl);                
-        
-        // give it 
+        curl_close($this->_curl);
+
+        // give it
         return $r;
-    
+
     }
-    
+
     // oauth
     protected function oauthRequest($url, $params=array(), $method="GET", $headers=array()) {
-        
+
         // if oauth isn't here
         if (!class_exists('OAuth')) { return false; }
-        
+
         // if we don't have oauth
         if (!$this->_oauth) {
-        
+
             // setup
             $this->_oauth = new \OAuth($this->auth['key'], $this->auth['secret'], OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_AUTHORIZATION);
             $this->_oauth->enableDebug();
-            
+
         }
-        
-        
+
+
         // token
         $this->_oauth->setToken($this->auth['key'], $this->auth['secret']);
-        
-        // fetch it 
+
+        // fetch it
         try {
             $this->_oauth->fetch($url, $params, $method, $headers);
         }
@@ -247,68 +247,68 @@ class webservice extends \bolt\plugin\factory {
                 "",
                 $e->getCode(),
                 array()
-            );        
+            );
         }
-    
+
         // info
-        $info = $this->_oauth->getLastResponseInfo(); 
-        
-        // return 
+        $info = $this->_oauth->getLastResponseInfo();
+
+        // return
         return new webserviceResponse(
             $this,
             $this->_oauth->getLastResponse(),
             $info['http_code'],
-            $info            
+            $info
         );
-    
+
     }
-    
+
 }
 
 class webserviceResponse {
-    
+
     private $_code = false;
     private $_body = false;
     private $_error = false;
     private $_info = array();
     private $_req = array();
-    
+
     public function __construct($req, $body, $code, $info) {
-    
+
         // set our body
         $this->_body = $body;
-        
+
         // code
         $this->_code = $code;
-        
+
         // info
         $this->_info = $info;
-        
+
         // req
-        $this->_req = $req;    
-    
+        $this->_req = $req;
+
     }
-    
+
     // magic
     public function __call($name, $args) {
-        
+
         switch($name) {
-            
+
             // erro
             case 'error':
             case 'getError':
                 return $this->_error;
-            
+
             // code
             case 'code':
             case 'getCode':
-                return $this->_code;            
-            
+                return $this->_code;
+
             // text
             case 'getText':
             case 'text':
                 return $this->_body;
-            
+
             // json
             case 'json':
             case 'getJson':
@@ -318,27 +318,27 @@ class webserviceResponse {
             case 'jsonObject':
             case 'getJsonObject':
                 return json_decode($this->_body);
-                
+
             // xml
             case 'xml':
             case 'getXml':
                 return simplexml_load_string($this->_body);
-                
+
             // info
             case 'info';
             case 'getInfo':
                 return $this->_info;
-                
+
             // req
             case 'request':
-            case 'getRequest':  
+            case 'getRequest':
                 return $this->_req;
-                    
+
         };
-    
+
         return false;
-    
+
     }
-    
+
 
 }
