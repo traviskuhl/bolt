8732edc7fd8895bbfbee5193327b25628bbf406c
Travis <travis@teamcoco.com>
2012-11-29 07:29:39 +0000

    added cookie prefix

--- src/bolt/browser/cookie.php
+++ src/bolt/browser/cookie.php
@@ -13,44 +13,45 @@ class cookie extends plugin\singleton {
     ///
     /// @param $url base url
     /// @param $params array of params to add
-    /// 
+    ///
     /// @return string url with additional params
     ////////////////////////////////////////////////
 	public function set($name, $value, $expires=false, $domain=false, $secure=false, $http=false) {
-	
+
 		// domain
 		$domain = b::config()->get("cookies.domain");
-	
+		$prefix = b::config()->get("cookies.prefix");
+
 		// get name from config
 		if ( substr($name, 0, 1) == '$' ) {
 			$name = b::_(substr($name, 1));
 		}
-	
+
 		// is value a string
 		if ( is_array($value) ) {
-		
+
 			// see if this cookie already exists
 			if ( p($name, false, $_COOKIE) ) {
-				
+
 				// current
 				$cur = $this->get($name);
-				
+
 				    // not an array
 				    if (!is_array($cur)) { $cur = array(); }
-				
+
 				// merge the values from the current cookie
 				$value = b::mergeArray( $cur, $value );
-				
+
 			}
-			
+
 			// encode it
 			$e = base64_encode(json_encode($value));
-		
+
 			// return the padded value
 			$value = ":".b::md5($e).$e;
-			
+
 		}
-		
+
 		// expires
 		if ($expires AND $expires{0} == '+') {
             $expires = strtotime($expires);
@@ -58,66 +59,68 @@ class cookie extends plugin\singleton {
 		else if ( $expires AND $expires < b::utctime() ) {
 			$expires = b::utctime() + $expires;
 		}
-	
-		// set it 
-		$r = setcookie($name, $value, $expires, '/', $domain, $secure, $http);
-		
-	
+
+		// set it
+		$r = setcookie($prefix.$name, $value, $expires, '/', $domain, $secure, $http);
+
+
 	}
-	
+
 	public function delete($name) {
 
 		// domain
 		$domain = b::config()->get("cookies.domain");
+		$prefix = b::config()->get("cookies.prefix");
 
 		// get name from config
 		if ( substr($name, 0, 1) == '$' ) {
 			$name = b::_(substr($name, 1));
 		}
 
-		// set it 
-		setcookie($name, false, time()+1, '/', $domain);
-	
+		// set it
+		setcookie($prefix.$name, false, time()+1, '/', $domain);
+
 	}
-	
+
 	public function get($name) {
 
-	
+		$prefix = b::config()->get("cookies.prefix");
+
 		// get name from config
 		if ( substr($name, 0, 1) == '$' ) {
 			$name = b::_(substr($name, 1));
 		}
-	
-		// try to get it 
-		$cookie = urldecode(p($name, false, $_COOKIE));
-	
+
+		// try to get it
+		$cookie = urldecode(p($prefix.$name, false, $_COOKIE));
+
 			// if we don't have it, stop
 			if ( !$cookie ) { return false; }
-			
+
 		// see if the first val is a :
 		if ( $cookie{0} == ':' ) {
-							
+
 			// make sure we're goof
 			$cookie = $this->getDecodedCookie($cookie);
-			
+
 		}
-	
-		// return it 
+
+		// return it
 		return $cookie;
-	
-	}		
-	
+
+	}
+
 	public function getDecodedCookie($cookie) {
 
-		// split it 
+		// split it
 		$sig = substr($cookie, 1, 32);
-		
+
 		// now the value
 		$e = substr($cookie, 33);
-		
+
 		// make sure we're goof
 		return ( b::md5($e) == $sig ? json_decode(base64_decode($e), true) : false );
-		
+
 	}
 
 
