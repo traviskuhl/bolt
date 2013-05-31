7368b23f8263f13fd28463d8984325561716ddc4
Travis <travis@kuhl.co>
2013-05-31 09:06:49 -0700

    no depend.json

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,22 +7,11 @@ define("bDevMode", true);
 define("bGlobalSettings", false);
 define("bLogLevel", 0);
 
-// get our depend file from build
-$depend = json_decode(file_get_contents(__DIR__."/../build/depend.json"), true);
-
 $_SERVER['HTTP_HOST'] = 'test.bolthq.com';
 define("bSelf", "http://test.bolthq.com");
 
 // include our bolt
 include_once(dirname(__FILE__)."/../src/bolt.php");
-
-// add depend
-foreach ($depend as $pkg) {
-    $dir = realpath(__DIR__."/../vendor/{$pkg['name']}/{$pkg['lib']}/");
-    if (file_exists($dir)) {
-        b::$autoload[] = dirname($dir);
-    }
-}
 
 // short
 define("INC", __DIR__."/include");
